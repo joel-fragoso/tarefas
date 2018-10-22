24c669ce1c32a2c3bb0f9956909c1ca18fbeee80
Joel Fragoso <joel.fragoso@hotmail.com>
2018-10-22 14:48:01 -0300

    Modificação do arquivo e criação de código PHP

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 	<head>
@@ -18,5 +19,26 @@
 				<input type="submit" value="Cadastrar">
 			</fieldset>
 		</form>
+		<?php
+			if (isset($_GET['nome'])) {
+				$_SESSION['lista_tarefas'][] = $_GET['nome'];
+			}
+
+			$lista_tarefas = array();
+
+			if (isset($_SESSION['lista_tarefas'])) {
+				$lista_tarefas = $_SESSION['lista_tarefas'];
+			}
+		?>
+		<table>
+			<tr>
+				<th>Tarefas</th>
+			</tr>
+			<?php foreach ($lista_tarefas as $tarefa) : ?>
+				<tr>
+					<td><?php echo $tarefa; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
 	</body>
 </html>
