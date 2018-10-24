bae13d72c0fd8cc456075bd01a23a71a6a559551
Joel Fragoso <joel.fragoso@hotmail.com>
2018-10-24 07:54:00 -0300

    Alteração no título do aplicativo

--- template.php
+++ template.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="IE-Edge, Chrome=1">
-		<title>Tarefas</title>
+		<title>Gerenciador de tarefas</title>
 		<link rel="stylesheet" href="assets/css/app.css">
 	</head>
 	<body>
